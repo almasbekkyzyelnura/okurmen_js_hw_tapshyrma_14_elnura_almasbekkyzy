9a1bc17b00ea37994f0e14bd2fb32a947ca131f4
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-03-19 23:16:09 +0600

    Added the

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+let counter = 0;
+setInterval(function () {
+    counter++; console.log(`Прошло: ${counter} сек`);
+}, 1000);
+
+    
+
+/*
+let counteer = 0;
+
+
+    const interval = setInterval(function (){
+      counteer++;
+    console.log(`Прошло: ${counteer} сек`);
+    if (counteer >= 5) {
+    clearInterval(interval);
+    }
+    }, 1000);     */
